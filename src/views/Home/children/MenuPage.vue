eaea05073b571c556013490d1c7a1046df4a08d1
wanxin520 <3113415005@qq.com>
2024-09-28 02:53:58 +0800

    修改了Menu圆形图标的样式

--- src/views/Home/children/MenuPage.vue
+++ src/views/Home/children/MenuPage.vue
@@ -11,19 +11,22 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
-        <van-swipe class="my-swipe" :stop-propagation:="false" :width="100" :height="100" :loop="false"
-            :show-indicators="false">
-            <van-swipe-item @click="isClicked(index)" class="scroll_item" v-for="(item, index) in props.data"
-                :key="item.name">
-                <div class="flex flex-col items-center">
-                    <img class="filter w-[3rem]" :src="item.iconUrl" alt="" />
-                </div>
-                <div class="text-[#818080] text-[14px] ml-6">
-                    {{ item.name }}
-                </div>
-            </van-swipe-item>
-        </van-swipe>
+    <div class="m-auto">
+        <div class="w-[100vw] flex justify-center items-center">
+            <van-swipe class="my-swipe" :width="90" :height="80" :stop-propagation="false" :loop="false"
+                :show-indicators="false">
+                <van-swipe-item @click="isClicked(index)" v-for="(item, index) in props.data" :key="item.name">
+                    <div class="flex flex-col items-center">
+                        <div class="">
+                            <img class="filter w-[2.6rem]" :src="item.iconUrl" alt="">
+                        </div>
+                        <div class="text-[#4d4c4c] text-[11px]">
+                            {{ item.name }}
+                        </div>
+                    </div>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
     </div>
 </template>
 
