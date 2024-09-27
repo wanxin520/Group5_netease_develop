d33e7f87d7124b4c6eccccbc5bd55fa85c16436f
wanxin520 <3113415005@qq.com>
2024-09-28 02:44:36 +0800

    完成底部tabBar组件渲染

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -19,7 +19,7 @@ const { data: pageData } = useRequest(getHomePageData)
             <BlockType v-for="item in pageData" :data="item" :key="item.blockCode" class="min-h-[100%]"></BlockType>
             <!-- <div style="height: 300px;"></div> -->
         </div>
-        <footer>
+        <footer class="fixed bottom-0 left-0 right-0 z-index-1008">
             <Bottom></Bottom>
         </footer>
     </div>
--- src/views/Home/children/Bottom.vue
+++ src/views/Home/children/Bottom.vue
@@ -1,12 +1,55 @@
 <script setup>
 import { ref } from "vue"
-
+import { Icon } from "@iconify/vue";
+const active = ref(0);
+const onChange = (index) => showToast(`标签 ${index}`);
 
 </script>
 
 <template>
-    <div>
-        <h3>底部TabBar组件</h3>
+    <div class="flex flex-col justify-end">
+        <van-tabbar v-model="active" fixed @change="onChange" inactive-color="gray" active-color="#ee0a24">
+            <van-tabbar-item>
+                <div class="flex flex-col items-center">
+                    <Icon icon="ri:netease-cloud-music-fill" width="1.6rem" />
+                    <div>
+                        <span>发现</span>
+                    </div>
+                </div>
+            </van-tabbar-item>
+            <van-tabbar-item>
+                <div class="flex flex-col justify-center items-center">
+                    <Icon icon="mynaui:radio" width="1.6rem" />
+                    <div>
+                        <span>播客</span>
+                    </div>
+                </div>
+            </van-tabbar-item>
+            <van-tabbar-item>
+                <div class="flex flex-col justify-center items-center">
+                    <Icon icon="mingcute:music-line" width="1.6rem" />
+                    <div class="flex flex-col justify-around items-center">
+                        <span>我的</span>
+                    </div>
+                </div>
+            </van-tabbar-item>
+            <van-tabbar-item>
+                <div class="flex flex-col justify-center items-center">
+                    <Icon icon="icon-park-outline:people-safe-one" width="1.6rem" />
+                    <div>
+                        <span>关注</span>
+                    </div>
+                </div>
+            </van-tabbar-item>
+            <van-tabbar-item>
+                <div class="flex flex-col justify-center items-center">
+                    <Icon icon="hugeicons:message-multiple-01" width="1.6rem" />
+                    <div>
+                        <span>社区</span>
+                    </div>
+                </div>
+            </van-tabbar-item>
+        </van-tabbar>
     </div>
 </template>
 <style scoped></style>
