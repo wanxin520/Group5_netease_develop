ff41747900ff180f32f2ed28fd9b97295a544cd7
wanxin520 <3113415005@qq.com>
2024-09-28 22:42:50 +0800

    完成全局路由配置，之后所有除登录页面之外其他所有数据获取都需要登录

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@
 */
 
 import { createWebHistory, createRouter } from "vue-router";
+import localforage from "localforage";
 import routes from "./routes";
 
 const router = createRouter({
@@ -12,7 +13,19 @@ const router = createRouter({
 
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
-  next();
-});
-
+  // console.log(to.fullPath);
+  // 如果还没有登录就跳转至登录页面
+  if (!to.fullPath.startsWith("/login")) {
+    localforage.getItem("userInfo")
+      .then((data) => {
+        data.cookie ? next() : next(`/login/phone?originPath=${to.fullPath}`)
+      })
+      .catch((err) => {
+        console.log(err)
+        next(`/login/phone?originPath=${to.fullPath}`)
+      })
+  } else {
+    next()
+  }
+})
 export default router;
