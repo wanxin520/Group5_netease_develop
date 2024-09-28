ed5f8beed1783b2f92724d7ab695f049849669ef
Shiyuquan <3174961601@qq.com>
2024-09-28 11:09:44 +0800

    完成了云村出品组件渲染

--- src/views/Home/children/YunCunProduced.vue
+++ src/views/Home/children/YunCunProduced.vue
@@ -1,16 +1,24 @@
 <script setup>
-  import { ref } from "vue"
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
+const router = useRouter();
+const props = defineProps({
+  data: {},
+});
 
+console.log(props.data.children);
+const isChecked = (id) => {
+  console.log(id);
+  let query = { id: id };
+  router.push({ name: "detail", query });
+};
 </script>
 
 <template>
-  <div>
-
+  <div class="w-[500vw] h-[50vw] bg-red-300 mt-20">
 
   </div>
 </template>
-<style scoped>
 
-
-</style>
+<style scoped></style>
