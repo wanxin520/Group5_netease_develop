3d2f3054d1557df8366520c22e49ce8cdc44cb91
liyanli <14858922+li-yanli-1@user.noreply.gitee.com>
2024-09-28 16:52:47 +0800

    完成了专属场景歌单

--- src/views/Home/children/BlockType.vue
+++ src/views/Home/children/BlockType.vue
@@ -12,12 +12,12 @@ import RCMDStyle from "./RCMDStyle.vue";
 const blockTypeComponentMap = {
   HOMEPAGE_BANNER: Banner, // 轮播图
   HOMEPAGE_BLOCK_OLD_DRAGON_BALL: MenuPage, // 圆形图标
-  HOMEPAGE_BLOCK_PLAYLIST_RCMD: RCMDPlayList,  //  推荐歌单
-  HOMEPAGE_BLOCK_STYLE_RCMD: RCMDStyle,  // 为你定制精选歌曲
+  HOMEPAGE_BLOCK_PLAYLIST_RCMD: RCMDPlayList, //  推荐歌单
+  HOMEPAGE_BLOCK_STYLE_RCMD: RCMDStyle, // 为你定制精选歌曲
   HOMEPAGE_BLOCK_MGC_PLAYLIST: MGCPlayList, //网易云音乐的雷达歌单
   HOMEPAGE_BLOCK_OFFICIAL_PLAYLIST: OfficialPlayList, //专属场景歌单
   HOMEPAGE_BLOCK_YUNCUN_PRODUCED: YunCunProduced, // 云村出品
-  HOMEPAGE_PODCAST24: BroadCast,  // 广播
+  HOMEPAGE_PODCAST24: BroadCast, // 广播
   // HOMEPAGE_BLOCK_NEW_HOT_COMMENT: HotComment, // 热评歌曲
 };
 const props = defineProps({
@@ -54,16 +54,16 @@ const propsData = computed(() => {
           playCount: item.resources[0].resourceExtInfo.playCount,
           highQuality: item.resources[0].resourceExtInfo.highQuality,
           hasListened: item.resources[0].resourceExtInfo.hasListened,
-          specialType: item.resources[0].resourceExtInfo.specialType
-        }))
-      }
+          specialType: item.resources[0].resourceExtInfo.specialType,
+        })),
+      };
     // 为你定制精选歌曲
     case "HOMEPAGE_BLOCK_STYLE_RCMD":
       // console.log(props.data)
       return {
         name: props.data.uiElement.subTitle.title,
-        creatives: props.data.creatives
-      }
+        creatives: props.data.creatives,
+      };
 
     // 网易云音乐的雷达歌单
     case "HOMEPAGE_BLOCK_MGC_PLAYLIST":
@@ -80,9 +80,9 @@ const propsData = computed(() => {
           playCount: item.resources[0].resourceExtInfo.playCount,
           highQuality: item.resources[0].resourceExtInfo.highQuality,
           hasListened: item.resources[0].resourceExtInfo.hasListened,
-          specialType: item.resources[0].resourceExtInfo.specialType
-        }))
-      }
+          specialType: item.resources[0].resourceExtInfo.specialType,
+        })),
+      };
     // 专属场景歌单
     case "HOMEPAGE_BLOCK_OFFICIAL_PLAYLIST":
       return {
@@ -98,9 +98,9 @@ const propsData = computed(() => {
           playCount: item.resources[0].resourceExtInfo.playCount,
           highQuality: item.resources[0].resourceExtInfo.highQuality,
           hasListened: item.resources[0].resourceExtInfo.hasListened,
-          specialType: item.resources[0].resourceExtInfo.specialType
-        }))
-      }
+          specialType: item.resources[0].resourceExtInfo.specialType,
+        })),
+      };
     // 云村出品
     case "HOMEPAGE_BLOCK_YUNCUN_PRODUCED":
       return {
@@ -110,7 +110,7 @@ const propsData = computed(() => {
           imageUrl: item.resources[0].uiElement.image.imageUrl,
           title: item.resources[0].uiElement.mainTitle.title,
         })),
-      }
+      };
     // 24小时广播
     case "HOMEPAGE_PODCAST24":
       // console.log(props.data);
@@ -119,18 +119,21 @@ const propsData = computed(() => {
         children: props.data.creatives[0].resources.map((item) => ({
           imageUrl: item.uiElement.image.imageUrl,
           title: item.uiElement.mainTitle.title,
-        }))
-      }
+        })),
+      };
     default:
       return null;
   }
   return [];
 });
-// console.log(props.data);
+console.log(props.data);
 </script>
 
 <template>
   <div>
-    <component :is="blockTypeComponentMap[props.data.blockCode]" :data="propsData"></component>
+    <component
+      :is="blockTypeComponentMap[props.data.blockCode]"
+      :data="propsData"
+    ></component>
   </div>
 </template>
