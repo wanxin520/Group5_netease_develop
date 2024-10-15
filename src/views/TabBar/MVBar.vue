b32f03bc3ea779b68632bbda2e43a5b509745694
wuyanli <3098745387@qq.com>
2024-10-15 11:27:32 +0800

    完成最新MV的渲染

--- src/views/TabBar/MVBar.vue
+++ src/views/TabBar/MVBar.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Bottom from "../Bottom/Bottom.vue";
 import BottomPlayBar from "@/components/play/music/BottomPlayBar.vue";
+import NewestMV from "../Pages/MV/NewestMV.vue";
 </script>
 
 <template>
   <div>
     <h1>视频</h1>
+  </div>
+  <div>
+    <NewestMV></NewestMV>
   </div>
   <div class="fixed bottom-[50px] left-0 right-0 z-index-1008">
     <BottomPlayBar></BottomPlayBar>
