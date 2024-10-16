812dfbd58206c12ddc19a0cf035c620d0d6ba7be
liyanli <14858922+li-yanli-1@user.noreply.gitee.com>
2024-10-16 16:29:46 +0800

    完成视频页面的 “日本” 页面渲染

--- src/views/Pages/MV/children/OfficialMV.vue
+++ src/views/Pages/MV/children/OfficialMV.vue
@@ -1,13 +1,10 @@
 <script setup>
-import { ref } from "vue"
-
-
+import { ref } from "vue";
 </script>
 
 <template>
-    <div>
-
-        <h1>网易视频</h1>
-    </div>
+  <div>
+    <h1>网易视频</h1>
+  </div>
 </template>
 <style scoped></style>
