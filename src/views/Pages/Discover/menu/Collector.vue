a7d5c5ffb5d939382a8b197bed5ed69a4c81af22
liyanli <14858922+li-yanli-1@user.noreply.gitee.com>
2024-10-16 20:16:56 +0800

    完成视频播放组件的“韩国”页面

--- src/views/Pages/Discover/menu/Collector.vue
+++ src/views/Pages/Discover/menu/Collector.vue
@@ -1,13 +1,23 @@
-<script setup>
-import { ref } from "vue"
+<!-- <script setup>
+// import { ref } from "vue";
+import { getCollector, getCollectorList } from "@/api";
+import { useRequest } from "vue-request";
+import { watch } from "vue";
 
-
+const { data: CollectorData } = useRequest(() => getCollector());
+const { data: CollectorDataList } = useRequest(() => getCollectorList());
+watch(CollectorData, () => {
+  console.log(CollectorData);
+});
+watch(CollectorData, () => {
+  console.log(CollectorDataList);
+});
 </script>
 
 <template>
-    <div>
-        <h1>收藏家</h1>
-
-    </div>
+  <div v-for="item in CollectorData" :key="item">
+    <div>{{ item._rawValue.data[0] }}</div>
+  </div>
+  <div v-for="item1 in CollectorDataList" :key="item1"></div>
 </template>
-<style scoped></style>
+<style scoped></style> -->
